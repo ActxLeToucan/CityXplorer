d4286fdde82e0148db463de59b51e28b623667b0
lopesvaz3u <lopesvaz3u@etu.univ-lorraine.fr>
2022-01-07 21:00:34 +0100

    création de la classe post

--- source/CityXplorer_lopes/lib/post.dart
+++ source/CityXplorer_lopes/lib/post.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
+import 'share_bar_icon.dart';
 
+// classe correspondant a la vue d un poste de l utilisateur
 class Post extends StatelessWidget{
   const Post({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Padding(
+      padding: const EdgeInsets.only(top: 12,),
+      child: Container(//le contenainer qui contient le poste
+          constraints: const BoxConstraints.expand(
+            width: 350,
+            height: 310,
+          ),
+          decoration: BoxDecoration(color: Colors.blueGrey),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const SizedBox(width: 100,),
+                  Text(//le nom du poste
+                    'Statue Blandan',
+                    style : TextStyle(fontWeight: FontWeight.bold,fontSize: 25),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {
+                      const snackBar = SnackBar(content: Text('afficher un mini menu qui fait des trucs'));
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    icon: const Icon(Icons.more_vert),
+                  )
+                ],
+              ),
+              Container(//la photo du poste
+                constraints: const BoxConstraints.expand(
+                  width: 300,
+                  height: 150,
+                ),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/antoine.jpg'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(//le nom du poste
+                  "Une statut d'un grand homme sur la place Blandan" ,
+                  style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.2),
+                ),
+              ),
+              ShareBar(),
+            ],
+          )
+      ),
     );
   }
 
