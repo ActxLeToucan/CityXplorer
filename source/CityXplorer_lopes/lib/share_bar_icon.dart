ad7b1e4dcc0d6bae430867881607d4c9c55c2a74
lopesvaz3u <lopesvaz3u@etu.univ-lorraine.fr>
2022-01-07 21:04:59 +0100

    création du composant share_bar_icon utilisé dans la création d'un poste d'utilisateur

--- source/CityXplorer_lopes/lib/share_bar_icon.dart
+++ source/CityXplorer_lopes/lib/share_bar_icon.dart
@@ -1,13 +1,48 @@
-
 import 'package:flutter/material.dart';
 
-/**
-
-class ShareBar extends StatefulWidget{
+class ShareBar extends StatefulWidget {
+  const ShareBar({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    State<ShareBar> createState() => _ShareBar();
-    throw UnimplementedError();
+  State<ShareBar> createState() => _ShareBarState();
+}
+
+class _ShareBarState extends State<ShareBar>{
+  bool _isFavorited = false;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+          icon: const Icon(Icons.share),
+          iconSize: 28,
+          color: Colors.black,
+          onPressed: () {
+            const snackBar = SnackBar(content: Text('partager'));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.map),
+          iconSize: 28,
+          color: Colors.black,
+          onPressed: () {
+            const snackBar = SnackBar(content: Text('carte'));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+        ),
+        IconButton(
+          icon: Icon(_isFavorited ? Icons.favorite : Icons.favorite_border),
+          iconSize: 28,
+          color: Colors.red[400],
+          onPressed: () {
+            setState(() {
+              _isFavorited = !_isFavorited;
+            });
+          },
+        ),
+      ],
+    );
   }
 }
