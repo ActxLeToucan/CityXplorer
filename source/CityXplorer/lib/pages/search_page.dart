0a0dad7940bbbec69fb2b8ad1ed293545a3bde9c
Antoine Contoux <antoinectx54@hotmail.com>
2022-02-07 17:06:23 +0100

    search bar

--- source/CityXplorer/lib/pages/search_page.dart
+++ source/CityXplorer/lib/pages/search_page.dart
@@ -2,8 +2,24 @@ import 'package:flutter/material.dart';
 
 import '../styles.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  List<int> _searchIndexList = [];
+  List<String> _list = [
+    'English Textbook',
+    'Japanese Textbook',
+    'English Vocabulary',
+    'Japanese Vocabulary',
+    'Lorem ipsum dolor sit amet.'
+  ];
+
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +32,48 @@ class SearchPage extends StatelessWidget {
             decoration: BoxDecoration(
                 color: Colors.white, borderRadius: BorderRadius.circular(5)),
             child: Center(
-              child: TextField(
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search),
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.clear),
-                      onPressed: () {
-                        //TODO
-                      },
-                    ),
-                    hintText: 'Rechercher un utilisateur...',
-                    border: InputBorder.none),
-              ),
+              child: _renderSearchBar(),
             ),
           )),
+      body: _searchListView(),
+    );
+  }
+
+  Widget _searchListView() {
+    return ListView.builder(
+        itemCount: _searchIndexList.length,
+        itemBuilder: (context, index) {
+          index = _searchIndexList[index];
+          return Card(child: ListTile(title: Text(_list[index])));
+        });
+  }
+
+  Widget _renderSearchBar() {
+    return TextField(
+      textInputAction: TextInputAction.search,
+      controller: _controller,
+      onSubmitted: (String text) {
+        setState(() {
+          _searchIndexList = [];
+          for (int i = 0; i < _list.length; i++) {
+            if (_list[i].toLowerCase().contains(text.toLowerCase())) {
+              _searchIndexList.add(i);
+            }
+          }
+        });
+      },
+      autocorrect: false,
+      autofocus: true,
+      decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.search),
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.clear),
+            onPressed: () {
+              _controller.text = "";
+            },
+          ),
+          hintText: 'Rechercher un utilisateur...',
+          border: InputBorder.none),
     );
   }
 }
