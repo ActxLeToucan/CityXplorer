1a1767e7b14a94cfff9630bd4ddbd1838620170a
Antoine Contoux <contoux1u@etu.univ-lorraine.fr>
2022-03-25 22:43:47 +0100

    httpS

--- source/CityXplorer/lib/conf.dart
+++ source/CityXplorer/lib/conf.dart
@@ -1,5 +1,5 @@
 class Conf {
-  static const String domainServer = "http://cityxplorer.streamlor.io";
+  static const String domainServer = "https://cityxplorer.streamlor.io";
   static const String apiPath = "/api";
   static const String googleApiKey = "AIzaSyCoZ5pkSaTZk3rpiGrm3yuTIj48y7NdncU";
 }
